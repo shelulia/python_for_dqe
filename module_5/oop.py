75a2a7b315e8ff9d0542d812520da4c76a8bcde2
SHALIYU1 <yuliya.shaliuta_ext@novartis.com>
2023-03-31 21:56:40 +0300

    fixed on comments

--- module_5/oop.py
+++ module_5/oop.py
@@ -1,5 +1,5 @@
 from oop_classes import PdqeNewsPaperSite
 
 # Note! please insert into parameter desired file location
-module_5 = PdqeNewsPaperSite("/Users/shaliyu1/Documents/python_for_dqe/module_5/newspaper.txt")
+module_5 = PdqeNewsPaperSite("./newspaper.txt")
 module_5.user_input()
